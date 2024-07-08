c23232529771e92b333a3f136eedc129139f634c
IjonK007 <kushta.joni@gmail.com>
2024-07-08 16:02:30 +0200

    Small changes + added env

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-24">
-      <Image src="/logo.png" alt="Trackr.gl" width={200} height={200} />
-      <h1 className="text-4xl font-bold">Trackr.gl</h1>
+    <main className="flex min-h-screen flex-col items-center justify-start p-24 gap-5">
+      <div className="flex flex-col items-center">
+        <Image src="/logo.png" alt="Trackr.gl" width={200} height={200} />
+        <h1 className="text-4xl font-bold">Trackr.gl</h1>
+      </div>
+      <div className="flex flex-col items-center justify-center md:text-xl text-md">
+        <p>Still in the works...</p>
+        <p>A web app for tracking your progress on your goals.</p>
+      </div>
     </main>
   );
 }
