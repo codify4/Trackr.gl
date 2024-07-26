347da3d9d1d1619311bc3f4fd193532d86c65bd5
codify4 <kushta.joni@gmail.com>
2024-07-26 16:26:14 +0200

    Changed habit counter

--- components/habitPage/HabitCounter.tsx
+++ components/habitPage/HabitCounter.tsx
@@ -1,10 +1,28 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+
 import { getHabitsCount } from '@/actions/actions';
 import { cn } from '@/lib/utils';
 
 import { Button } from '../ui/button';
 
-const HabitCounter = async ({ className }: { className?: string }) => {
-    const count = await getHabitsCount();
+const HabitCounter = ({ className }: { className?: string }) => {
+    const [count, setCount] = useState<number>();
+
+    const fetchHabitsCount = async () => {
+        try {
+            const habitsCount = await getHabitsCount();
+            setCount(habitsCount);
+            console.log(habitsCount);
+        } catch (err: any) {
+            console.error(err.message);
+        }
+    };
+
+    useEffect(() => {
+        fetchHabitsCount();
+    }, []);
 
     return (
         <Button variant="ghost" className={cn('flex items-center', className)}>
